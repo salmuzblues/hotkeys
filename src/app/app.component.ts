88d0d74e0df65f0d6c56d8abcec7e1075bf81cef
CarranzaAlarconJuan <salmuzblues@gmail.com>
2017-05-25 15:59:40 -0500

    finish the commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'HK-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  host: {'(window:keydwon)': 'HK($event)'},
+  template: `
+    <button (click)=showMessage()>Hotkey Test</button>
+    
+  `
 })
 export class AppComponent {
-  title = 'HK works!';
+  // As you can see I’ve used the element host to add a listener to the window:keydown event,
+  //  it will call the function hotkeys(event) every time this event occurs, all we have to do now on
+  // this function is to determine which key was pressed and take actions accordingly.
+  HK (event) {
+    if ( event.KeyCode === 65 && event.ctrlKey ) {
+      this.showMessage();
+    }
+  }
+ // this is for showing us the message.
+  showMessage() {
+    alert('Alex Test');
+}
 }
